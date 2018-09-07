8c7fd92187080100cb98cc428425bbe1661daaa1
UCGv2 <chrisglennii@yahoo.com>
2018-09-06 23:58:14 -0500

    comments and headers

--- include/SimpleArray.h
+++ include/SimpleArray.h
@@ -1,22 +1,56 @@
 // @author G. Hemingway, copyright 2018 - All rights reserved
 //
+// File name: SimpleArray.h
+// Author: Christopher Glenn II
+// VUnetid: glennca1
+// Email: christopher.a.glenn@vanderbilt.edu
+// Class: CS3251
+// Date: 9/7/18
+// Honor statement: I have neither given nor received unauthorized help on this assignment
+// Assignment Number: 01
+// Description: This is the declaration of a SimpleArray using AllocationTracker objects
+
 #ifndef SIMPLE_ARRAY_H
 #define SIMPLE_ARRAY_H
 
 // Just include the special AllocationTracker class
 #include "AllocationTracker.h"
 #include <stdexcept>
-
+/**
+ * @brief Simulates a simplified array structure
+ * @details Simulates the referential nature of arrays by pointing an AllocationTracker* to an
+ * array of AllocationTracker objects. SimpleArrays can be constructed but not assigned or copied.
+ */
 class SimpleArray {
 public:
     /*** Add some constructors/destructor goodness here ***/
+
+    /**
+     * @brief Default constructor for the SimpleArray class
+     */
     SimpleArray();
 
+    /**
+     * @brief Overloaded Constructor for the SimpleArray class
+     */
     SimpleArray(AllocationTracker* a);
+
+    /**
+     *
+     * @param noCopy
+     */
     SimpleArray(SimpleArray& noCopy) = delete;
 
+    /**
+     * @brief Destructor for the Simple Array class
+     */
     ~SimpleArray();
 
+    /**
+     *
+     * @param noAssign
+     * @return
+     */
     SimpleArray& operator=(const SimpleArray& noAssign) = delete;
 
     /**
@@ -30,18 +64,59 @@ public:
 
     /*** Need to add some things here ***/
 
+    /**
+     * @brief Fetches mArray
+     * @return mArray
+     */
     const AllocationTracker* get() const;
+
+    /**
+     *@brief Returns a SimpleArray to its default settings
+     * @details Returns a Simple Array to its default settings by freeing
+     * any memory used by AllocationTracker objects that mArray points to and
+     * sets mArray to nullptr
+     */
     void reset();
+
+    /**
+     * @brief Deletes the current array in mArray and replaces it with parameter "r"
+     * @param r is a new array or nullptr
+     * @post mArray points to parameter "r"
+     */
     void reset(AllocationTracker* next);
+    /**
+     * @brief Swaps the mArray fields of two different SimpleArrays
+     * @details Swaps the mArray fields of two SimpleArrays provided their mArray fields
+     * are pointing to different locations. If they are the same, nothing needs to happen.
+     * @param a2 is a different SimpleArray
+     */
     void swap(SimpleArray& a2);
+
+    /**
+     * @brief Returns the AllocationTracker* associated with mArray and mArray
+     * is pointed to nullptr
+     * @return Returns the former address that mArray referenced
+     */
     AllocationTracker* release();
+    /**
+     * @brief returns whether or not mArray equals nullptr
+     * @return the evaluation of the equivalence between mArray and nullptr
+     */
     bool isNonNull() const;
 
 private:
     /*** Maybe some things here too ***/
 
     // Data members
+    /**
+     * @brief a pointer that points to an AllocationTracker array
+     */
     AllocationTracker* mArray;
+
+    /**
+     * @brief Deletes the array and assigns mArray to the appropriate next pointer
+     * @param nextPtr is the next address that mArray will point to
+     */
     void deleteOp(AllocationTracker* nextPtr);
 };
 
--- src/SimpleArray.cpp
+++ src/SimpleArray.cpp
@@ -1,13 +1,25 @@
-//
-// Created by fours on 8/29/2018.
-//
-#include "SimpleArray.h"
+// File name: SimpleArray.cpp
+// Author: Christopher Glenn II
+// VUnetid: glennca1
+// Email: christopher.a.glenn@vanderbilt.edu
+// Class: CS3251
+// Date: 9/7/18
+// Honor statement: I have neither given nor received unauthorized help on this assignment
+// Assignment Number: 01
+// Description: This is an implementation of a SimpleArray using AllocationTracker objects
 
+#include "SimpleArray.h"
+/**
+ * @brief Default constructor for the SimpleArray class
+ */
 SimpleArray::SimpleArray()
     : mArray(nullptr)
 {
 }
 
+/**
+ * @brief Overloaded Constructor for the SimpleArray class
+ */
 SimpleArray::SimpleArray(AllocationTracker* a)
     : mArray(nullptr)
 {
@@ -15,29 +27,67 @@ SimpleArray::SimpleArray(AllocationTracker* a)
         mArray = a;
     }
 }
-
+/**
+ * @brief Destructor for the Simple Array class
+ */
 SimpleArray::~SimpleArray()
 {
     deleteOp(nullptr);
 }
 
-const AllocationTracker* SimpleArray::get() const
-{
-    return mArray;
-}
+/**
+ * @brief Returns the reference to the array member
+ * @details Returns the reference to the member that corresponds to "i" in the array that
+ * mArray points to
+ * @param i functions like the index of an array
+ * @return Returns the reference to the AllocationTracker Array member
+ * that corresponds to parameter "i"
+ * @throws std::out_of_range- i tries to access members outside of the range of the array
+ */
 AllocationTracker& SimpleArray::getReference(const uint32_t i) const
 {
-    if (i > mArray->getCount()) {
+    if (i >= mArray->getCount()) {
         throw std::out_of_range("Out of Range ERROR");
     } else {
         return mArray[i];
     }
 }
 
-bool SimpleArray::isNonNull() const
+/**
+ * @brief Fetches mArray as a pointer and it is left const because it should not be changed
+ * @return mArray
+ */
+const AllocationTracker* SimpleArray::get() const
 {
-    return mArray != nullptr;
+    return mArray;
 }
+
+/**
+ *@brief Returns a SimpleArray to its default settings
+ * @details Returns a Simple Array to its default settings by freeing
+ * any memory used by AllocationTracker objects that mArray points to and
+ * sets mArray to nullptr
+ */
+void SimpleArray::reset()
+{
+    deleteOp(nullptr);
+}
+/**
+ * @brief Deletes the current array in mArray and replaces it with parameter "r"
+ * @param r is a new array or nullptr
+ * @post mArray points to parameter "r"
+ */
+void SimpleArray::reset(AllocationTracker* r)
+{
+    deleteOp(r);
+}
+
+/**
+ * @brief Swaps the mArray fields of two different SimpleArrays
+ * @details Swaps the mArray fields of two SimpleArrays provided their mArray fields
+ * are pointing to different locations. If they are the same, nothing needs to happen.
+ * @param a2 is a different SimpleArray
+ */
 void SimpleArray::swap(SimpleArray& a2)
 {
     if (a2.mArray != mArray) {
@@ -46,23 +96,31 @@ void SimpleArray::swap(SimpleArray& a2)
         a2.mArray = holder;
     }
 }
-void SimpleArray::reset()
-{
-    deleteOp(nullptr);
-}
-void SimpleArray::reset(AllocationTracker* r)
-{
-    deleteOp(r);
-}
+/**
+ * @brief Returns the AllocationTracker* associated with mArray and mArray
+ * is pointed to nullptr
+ * @return Returns the former address that mArray referenced
+ */
 AllocationTracker* SimpleArray::release()
 {
     AllocationTracker* copy = mArray;
     mArray = nullptr;
     return copy;
 }
-
-void SimpleArray::deleteOp(AllocationTracker* a)
+/**
+ * @brief returns whether or not mArray equals nullptr
+ * @return the evaluation of the equivalence between mArray and nullptr
+ */
+bool SimpleArray::isNonNull() const
+{
+    return mArray != nullptr;
+}
+/**
+ * @brief Deletes the array and assigns mArray to the appropriate next pointer
+ * @param nextPtr is the next address that mArray will point to
+ */
+void SimpleArray::deleteOp(AllocationTracker* nextPtr)
 {
     delete[](mArray);
-    mArray = a;
+    mArray = nextPtr;
 }
